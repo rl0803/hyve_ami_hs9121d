8a71b3347aa37dfb25f70f6f8b8e152f4d953c5d
Alan.W <alanw@hyvedesignsolutions.com>
2022-01-06 17:02:39 +0800

    Update the HyvePlatformSensorNum.h
    Changed:
        1. define sensor number for the voltage sensors
        2. define PMBus alert sensor number

--- packages/project/packages/hyveplatform_dev-src/data/HyvePlatformSensorNum.h
+++ packages/project/packages/hyveplatform_dev-src/data/HyvePlatformSensorNum.h
@@ -27,10 +27,7 @@
 #define SENSOR_NUM_BUTTON										(0x06)
 #define SENSOR_NUM_SYS_RESTART									(0x07)
 
-
-
-
-
+#define SENSOR_NUM_PMBUS_ALERT									(0x29)
 // PSU0
 #define SENSOR_NUM_PSU0_STATUS									(0x30)
 #define SENSOR_NUM_CURR_PSU0CIN									(0x31)
@@ -50,7 +47,22 @@
 #define SENSOR_NUM_TEMP_PSU1									(0x3E)
 #define SENSOR_NUM_TACH_PSU1Fan0								(0x3F)
 
-
+// Voltage
+#define SENSOR_NUM_VOLT_P12V									(0x40)
+#define SENSOR_NUM_VOLT_PDBP12V									(0x41)
+#define SENSOR_NUM_VOLT_PDBP3V3									(0x42)
+#define SENSOR_NUM_VOLT_P3V3									(0x43)
+#define SENSOR_NUM_VOLT_P0VDD3V3								(0x44)
+#define SENSOR_NUM_VAUX_P5V										(0x45)
+#define SENSOR_NUM_VOLT_P0VDD1V8								(0x46)
+#define SENSOR_NUM_VOLT_BATP3V									(0x47)
+#define SENSOR_NUM_VOLT_P0VDD1V1								(0x48)
+#define SENSOR_NUM_VOLT_P0VDDIO									(0x49)
+#define SENSOR_NUM_VOLT_P0VDDSOC								(0x4A)
+#define SENSOR_NUM_VOLT_P0VDDCORE0								(0x4B)
+#define SENSOR_NUM_VOLT_P0VDDCORE1								(0x4C)
+#define SENSOR_NUM_VAUX_P12V									(0x4D)
+#define SENSOR_NUM_VAUX_P3V3									(0x4E)
 
 
 
