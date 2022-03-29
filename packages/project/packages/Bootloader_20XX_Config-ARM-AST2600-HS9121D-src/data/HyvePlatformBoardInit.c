e294106d024f88ed61142283decb580389b7cee5
Alan.W <alanw@hyvedesignsolutions.com>
2022-03-29 13:10:51 +0800

    [BugFix] Fix BMC kernel boot failed, if the previous  BMC warm reboot during BIOS POST
    RootCause:
        The snoop controller is still working even the BMC chip reset,
        that will cause the kernel soc init stuck and the chip hang
    
    Solution:
        Always disable snoop controller in uboot,
        snoop funtion will be configured by kernel snoop dirver
    
    Tested:
       Step1.  ipmitool power on; ipmitool mc reset cold
       Step2. Check if the BMC can boot successfully

--- packages/project/packages/Bootloader_20XX_Config-ARM-AST2600-HS9121D-src/data/HyvePlatformBoardInit.c
+++ packages/project/packages/Bootloader_20XX_Config-ARM-AST2600-HS9121D-src/data/HyvePlatformBoardInit.c
@@ -16,9 +16,20 @@
 
 
 
+#define REG_HICR5						(0x1E789080)
+
 
 int HyvePlatformBoardInit(void)
 {
     printf("[%s] ...\n",__FUNCTION__);
+
+/* To fix kernel SOC init fail causing by snoop controller not disabled,
+	 if previous BMC warm reboot during BIOS POST
+	 Disable
+		 1. snoop & snoop IRQ
+		 2. ACCEPT response code for snoop #0 commands
+		 3. port 80 to SGPIO/GPIO                                       */
+	*(volatile u32 *)(REG_HICR5) &= (~0xC0004003);
+
     return 0;
 }
